e002334951fb907dafde836b50a39f8f858672aa
Joseph Kubiak <joseph.kubiak@gmail.com>
2017-08-06 21:43:09 -0700

    Initial commit.

---
+++ Assets/ElectroMagnet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectroMagnet : MonoBehaviour 
+{
+    [Range(4.0f, 2000.0f)]
+    public float MagnetForce;
+
+    Magnet[] m_magnets;
+
+    void OnValidate()
+    {
+        if (m_magnets == null)
+            return;
+
+        foreach(var m in m_magnets)
+        {
+            m.MagnetForce = MagnetForce;
+        }
+    }
+
+	void Start () 
+	{
+        m_magnets = GetComponentsInChildren<Magnet>();
+    }
+	
+	void Update () 
+	{
+		
+	}
+}
